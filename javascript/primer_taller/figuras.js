8e8b7f37d4e5827601532ec8bcfb8a84122b6b33
Diegofergamboa <diegofergamboa@hotmail.es>
2021-11-19 16:10:53 -0500

    Update figuras.js

--- javascript/primer_taller/figuras.js
+++ javascript/primer_taller/figuras.js
@@ -1,3 +1,17 @@
-const ladoCuadrado = 5;
+// Código del cuadrado
 
+const ladoCuadrado = 5;
 console.log('los lados del cuadrado son: ' + (ladoCuadrado * 4));
+
+// Triángulo 
+
+const ladoTriangulo1 = 6 ;
+const ladoTriangulo2 = 6 ;
+const baseTriángulo = 4 ;
+
+console.log("Los lados de mi triángulo son " 
+            + ladoTriangulo1 
+            + "y, " + ladoTriangulo2 
+            + "la base es: " 
+            + baseTriángulo
+            );

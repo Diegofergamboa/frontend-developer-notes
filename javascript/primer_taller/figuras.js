ad9ef2763039411d61fb7cab9404a8079009e75c
Diegofergamboa <diegofergamboa@hotmail.es>
2021-11-20 18:59:39 -0500

    Updating interacciones con el DOM a través de JS

--- javascript/primer_taller/figuras.js
+++ javascript/primer_taller/figuras.js
@@ -1,30 +1,24 @@
 // Código del cuadrado
 console.group("Cuadrado");
 
-// const ladoCuadrado = 5;
-// console.log('los lados del cuadrado son: ' + (ladoCuadrado * 4));
-
 function perimetroCuadrado(lado) {
-    return lado * 4;
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+    
+    const perimetro = perimetroCuadrado(value);
 }
-// perimetroCuadrado(10);
+
 
 
 function areaCuadrado(lado) {
     return lado * lado;
 }
-// areaCuadrado(156)
+
 
 console.groupEnd();
 
-// Triángulo 
-
 console.group("Triángulos");
 
-// const ladoTriangulo1 = 6 ;
-// const ladoTriangulo2 = 6 ;
-// const baseTriángulo = 4 ;
-// const alturaTriángulo = 5 ;
 
 function areaTriangulo (baseTriangulo, alturaTriangulo) {
     return ( baseTriangulo * alturaTriangulo ) / 2 ;
@@ -36,34 +30,13 @@ function perimetroTriangulo (ladoTriangulo1, ladoTriangulo2, baseTriangulo) {
     return c
 }
 
-// console.log(`El area del triangulo es ${areaTriangulo}`);
-// console.log(`El perímetro del cuadrado es ${perimetroTriangulo}`);
-
 console.groupEnd();
-
-// Código del círculo
-
-// console.group("Circulos");
-
-//Radio
-
-// Solicitado al user
-
-//Diametro
 
 function diametroCirculo (radio) {
     return radio * 2;
 }
 
-// //PI
-
 const PI = Math.PI;
-
-// console.log(`PI es equivalente a ${PI}`);
-
-//Circunferencia
-
-// const perimetroCirculo = diametroCirculo * PI ;
 
 function perimetroCirculo(radio) {
     const diametro = diametroCirculo(radio);
@@ -85,3 +58,7 @@ function areaCirculo(radio) {
 //     const sum = suma(1, 2);
 //     return c * sum ;
 // }
+
+// Calcular perimetro cuadrado
+
+

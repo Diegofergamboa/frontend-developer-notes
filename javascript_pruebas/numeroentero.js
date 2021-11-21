5aa093c29b37d8cb12d9ff8e05ac63bf1f96be56
Diegofergamboa <diegofergamboa@hotmail.es>
2021-11-20 20:09:06 -0500

    Update numeroentero.js

--- javascript_pruebas/numeroentero.js
+++ javascript_pruebas/numeroentero.js
@@ -3,4 +3,27 @@
 
 // 1. Tomar el valor de los enteros y colocarlos en una variable.
 // 2. Tomar el último dígito de cada una de las variables.
-3. 
+// 3. Comparar los los últimos dígitos
+// 4. Printear cuál de las dos variables es la mayor yield, si son iguales printear ese result 
+
+// Declaración de variables.
+
+const num1 = 1567 ;
+const num2 = 14534 ;
+
+const convertPositive = (a) => {
+    if (a < 0) {
+        return a * (-1);
+    } else {
+        return a ;
+    }
+}
+convertPositive(num1);
+convertPositive(num1);
+
+
+
+const udnm1 = (num1) => {
+
+} 
+const udnm2 =

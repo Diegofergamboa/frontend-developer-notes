5830f4ce8f41f8080fde0e93abdd99d54b0ff0a3
Diegofergamboa <diegofergamboa@hotmail.es>
2021-11-20 19:33:57 -0500

    Create numeroentero.js

---
+++ javascript_pruebas/numeroentero.js
@@ -0,0 +1,6 @@
+// Escribir un algoritmo que tome dos numeros y, determine cuál de los dos últimos dígitos es el mayor.
+
+
+// 1. Tomar el valor de los enteros y colocarlos en una variable.
+// 2. Tomar el último dígito de cada una de las variables.
+3. 

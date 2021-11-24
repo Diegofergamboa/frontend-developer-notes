8ffb11006bfb284ecf6c9501724928933696fcda
Diegofergamboa <diegofergamboa@hotmail.es>
2021-11-23 19:12:59 -0500

    Update tamano.js

--- javascript_pruebas/tamano.js
+++ javascript_pruebas/tamano.js
@@ -1,9 +1,12 @@
-const a = 10000;
-const cd = 0;
+// const a = 10000;
+
 function contador(a) {
+    let cd = 0;
     while (a > 0) {
         a = a / 10 ;
+        a = parseInt(a);
         cd = cd + 1 ;
+        console.log(`El numero de digitos del numero es ${cd}`);
     }
 }
 contador(100000);

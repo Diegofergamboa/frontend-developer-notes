f8589c1ac20fa000a987e330f3884fd5a30963c2
Diegofergamboa <diegofergamboa@hotmail.es>
2021-11-24 18:40:33 -0500

    Creado objeto con data de cupones

--- segundo taller/cupones.js
+++ segundo taller/cupones.js
@@ -1,1 +1,24 @@
-alert('Working on!')
+// Array con cupones
+
+const cupones = [
+    {
+        code : 'ASDF01' ,
+        discount : 10
+    } ,
+    {
+        code : 'ASDF02' ,
+        discount : 20
+    } ,
+    {
+        code : 'ASDF03' ,
+        discount : 30
+    } ,
+    {
+        code : 'ASDF04' ,
+        discount : 40
+    } ,
+    {
+        code : 'ASDF05' ,
+        discount : 50
+    }
+]

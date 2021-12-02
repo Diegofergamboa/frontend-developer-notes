fe9ad9bf4ee634171d13e49afbb6fbea0b136293
Diegofergamboa <diegofergamboa@hotmail.es>
2021-12-02 18:08:20 -0500

    updated

--- cuartotaller/api.js
+++ cuartotaller/api.js
@@ -1,17 +1,30 @@
+
 const salariosCol = colombia.map(
-    function (person) {
-        return person.salary;
+    function (personita) {
+        return personita.salary;
     }
 );
 
 const salariosColSorted = salariosCol.sort(
-    function (a, b) {
+    function (salaryA, salaryB) {
         return salaryA - salaryB;
     }
 );
 
 function esPar(number) {
     return (number % 2 === 0);
+}
+
+function calcularMediaAritmetica (array) {
+    const sumaLista = array.reduce (
+        function (valorAcumulado = 0, nuevoElemento ) {
+            return valorAcumulado + nuevoElemento ;
+        }
+)
+
+const promedioArray= sumaLista / array.length ;
+return promedioArray;
+
 }
 
 function medianaSalarios(lista) {
@@ -21,27 +34,31 @@ function medianaSalarios(lista) {
         const personitaMitad1 = lista[mitad - 1];
         const personitaMitad2 = lista[mitad];
 
-        const mediana = calcularMediaAritmetica(personitaMitad1, personitaMitad2);
+        const mediana =  calcularMediaAritmetica([personitaMitad1, personitaMitad2]);
+        
         return mediana;
 
     } else {
-        const personitaMitad = lista[mitad];
+        const personitaMitad = lista[mitad].salary;
         return personitaMitad;
     }
 }
 
+const medianaGeneralCol = medianaSalarios(salariosColSorted);
 
-function calcularMediaAritmetica (array) {
-    const sumaLista = array.reduce (
-        function (valorAcumulado = 0, nuevoElemento ) {
-            return valorAcumulado + nuevoElemento ;
-        }
-    )
 
-    const promedioArray= sumaLista / array.length ;
-    return promedioArray;
-}
+
+
+// ! Mediana del top 10%
+const spliceStart = (salariosColSorted.length * 90) / 100;
+const spliceCount = salariosColSorted.length - spliceStart;
+
+const salariosColTop10 = salariosColSorted.splice(spliceStart, spliceCount);
+
+const medianaTop10Col = medianaSalarios(salariosColTop10);
+
 
 console.log(
-    medianaSalarios(salariosColSorted)
+    medianaGeneralCol ,
+    medianaTop10Col
 )

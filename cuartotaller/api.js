23f5cfc8eaf878501cc0151b51f6a6b983e67ce6
Diegofergamboa <diegofergamboa@hotmail.es>
2021-12-02 16:10:15 -0500

    Update api.js

--- cuartotaller/api.js
+++ cuartotaller/api.js
@@ -16,4 +16,32 @@ function esPar(number) {
 
 function medianaSalarios(lista) {
     const mitad = parseInt(lista.length / 2);
+
+    if(esPar(lista.length)) {
+        const personitaMitad1 = lista[mitad - 1];
+        const personitaMitad2 = lista[mitad];
+
+        const mediana = calcularMediaAritmetica(personitaMitad1, personitaMitad2);
+        return mediana;
+
+    } else {
+        const personitaMitad = lista[mitad];
+        return personitaMitad;
+    }
 }
+
+
+function calcularMediaAritmetica (array) {
+    const sumaLista = array.reduce (
+        function (valorAcumulado = 0, nuevoElemento ) {
+            return valorAcumulado + nuevoElemento ;
+        }
+    )
+
+    const promedioArray= sumaLista / array.length ;
+    return promedioArray;
+}
+
+console.log(
+    medianaSalarios(salariosColSorted)
+)
